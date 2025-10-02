a2fba85143fcf5c8c678d7ac42ea794d63905241
armyrob22 <armyrob2260@yahoo.com>
2025-10-01 20:39:44 -0500

    i added logging functions to my monitor with time stamps.

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import psutil
 import time
+import logging
 
+logging.basicConfig(
+    filename='resource_monitor.log', 
+    level=logging.WARNING,
+    format= '%(asctime)s, %(levelname)s, %(message)s'
+)
 while True:
     time.sleep(1)
     cpu_usage = psutil.cpu_percent(interval=1,)
     if cpu_usage >= 75:
-        print("Cpu usage is to high!")
+        logging.warning("Cpu usage is to high!")
     per_cpu = psutil.cpu_percent(interval=1, percpu=True)   
     print("CPU Usage:", cpu_usage)
     print("Per CPU Usage:", per_cpu)
     memory_usage = psutil.virtual_memory().percent
-    if memory_usage >= 80:
-        print("memory at critical limit!")
+    if memory_usage >= 75:
+        logging.warning("memory at critical limit!")
     print("memory usage:", memory_usage)
     disk_usage = psutil.disk_usage('C:').percent
-    if disk_usage >= 60:
-        print("You better clean your hard drive sir!")
+    if disk_usage >= 75:
+        logging.warning("You better clean your hard drive sir!")
     print("disk usage:", disk_usage)
