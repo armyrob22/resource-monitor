233a563c58c0892b8f231aa6b560c52526199499
armyrob22 <armyrob2260@yahoo.com>
2025-10-02 11:34:19 -0500

    i added cpu freq monitoring. as well as network traffic data plus address data.

--- main.py
+++ main.py
@@ -12,9 +12,11 @@ while True:
     cpu_usage = psutil.cpu_percent(interval=1,)
     if cpu_usage >= 75:
         logging.warning("Cpu usage is to high!")
-    per_cpu = psutil.cpu_percent(interval=1, percpu=True)   
+    per_cpu = psutil.cpu_percent(percpu=True)
+    cpu_freq = psutil.cpu_freq(percpu=True)
     print("CPU Usage:", cpu_usage)
     print("Per CPU Usage:", per_cpu)
+    print("CPU frequency:", cpu_freq)
     memory_usage = psutil.virtual_memory().percent
     if memory_usage >= 75:
         logging.warning("memory at critical limit!")
@@ -23,3 +25,7 @@ while True:
     if disk_usage >= 75:
         logging.warning("You better clean your hard drive sir!")
     print("disk usage:", disk_usage)
+    network = psutil.net_io_counters(pernic=False, nowrap=True)
+    print("Network:", network)
+    network_addr = psutil.net_if_addrs()
+    print("network address's:", network_addr)
